def3ee1764acadd4c2bc2d21cfc4c9fa8ab811ed
Belay <belaybirlie.yimer@manchester.ac.uk>
2023-01-06 11:56:22 +0000

    documentation

--- R/utils.r
+++ R/utils.r
@@ -1,65 +1,39 @@
 #' Print a summary of the estimated causal mediation model
 #'
-#' @param fit A \code{stanfit} object obtained from \code{bayesgmed()}
-#' @param level "Confidence" level; Defines the limits of the credible intervals.
-#' Defaults to .95 (i.e. displays 95\% CIs.)
-#' @param pars Parameters to summarize. Defaults to main causal effect estimands
-#' similar to the \code{mediation} r package. See Details for more information.
-#' @param digits How many decimal points to display in the output. Defaults to 3.
+#' @param model A `stanfit` object obtained from `bayesgmed()`.
+#' @param level The "confidence" level that defines the limits of the credible intervals (default is .95, i.e. 95% CIs).
+#' @param pars The parameters to summarize (defaults to main causal effect estimands similar to the `mediation` package; see Details).
+#' @param digits The number of decimal points to display in the output (default is 3).
 #'
-#' @return A \code{data.frame} summarizing the estimated causal
-#' mediation model:
-#' \describe{
-#'  \item{Parameter}{Name of parameters}
-#'  \item{Mean}{Mean of parameter's posterior distribution.}
-#'  \item{Median}{Median of parameter's posterior distribution.}
-#'  \item{SE}{Standard deviation of parameter's posterior distribution.}
-#'  \item{ci_lwr}{The lower limit of Credible Intervals.}
-#'  \item{ci_upr}{The upper limit of Credible Intervals.}
-#'  \item{n_eff}{Number of efficient samples.}
-#'  \item{Rhat}{A value of 1.00 suggests model convergency}
-#'}
+#' @return A `data.frame` summarizing the estimated causal mediation model, including the following columns:
+#'   - `Parameter`: the name of the parameter.
+#'   - `Mean`: the mean of the parameter's posterior distribution.
+#'   - `Median`: the median of the parameter's posterior distribution.
+#'   - `SE`: the standard deviation of the parameter's posterior distribution.
+#'   - `ci_lwr`: the lower limit of the credible interval.
+#'   - `ci_upr`: the upper limit of the credible interval.
+#'   - `n_eff`: the number of efficient samples.
+#'   - `Rhat`: a value of 1.00 suggests model convergence.
 #'
-#' @details After estimating a model  with \code{bayesgmed()}, show the estimated results
-#' by using \code{bayesgmed_summary(fit)}, where \code{fit} is an object containing
-#' the fitted model.
+#' @details After estimating a model with `bayesgmed()`, use `bayesgmed_summary(fit)` to display the estimated results,
+#'   where `fit` is an object containing the fitted model. By default, `bayesgmed_summary()` only displays a subset of
+#'   the estimated parameters:
+#'     - `NDE_control`: direct effect estimate when the exposure level is set to the control value.
+#'     - `NDE_treated`: direct effect estimate when the exposure level is set to the treated value.
+#'     - `NIE_control`: mediated effect estimate when the exposure level is set to the control value.
+#'     - `NIE_treated`: mediated effect estimate when the exposure level is set to the treated value.
+#'     - `ANDE`: average direct effect of X on Y.
+#'     - `ANIE`: average indirect effect of X on Y.
+#'     - `TE`: the total effect of A on Y.
+#'   To display all estimated parameters where all chains merged, set `pars = NULL`. This will print all parameters defined
+#'   in the model definitions, including the most important ones:
+#'     - `alphaZ[]`: parameter estimate of the confounders (i.e., X -> Y) relationship, listed in the order they are specified
+#'       in the `covariates` argument of `bayesgmed()`. `alpha[1]` is the intercept.
+#'     - `alphaM`: parameter estimate of the M -> Y relationship.
+#'     - `alphaA`: parameter estimate of the A -> Y relationship.
+#'     - `betaZ`: parameter estimate of the confounders (i.e., X -> M) relationship, listed in the order they are specified
+#'       in the `covariates` argument of `bayesgmed()`. `beta[1]` is the intercept 
 #'
-#' The function shows, for each parameter specified with \code{pars},
-#' the posterior mean, and limits of the Credible Interval as specified
-#' by \code{level}. 
-#'
-#' \subsection{Parameters}{
-#' By default, \code{bayesgmed()} estimates and returns a large number of parameters
-#' and their associated standard deviations.
-#' However, \code{bayesgmed_summary()} by default only displays a subset of the
-#' estimated parameters:
-#'
-#' \describe{
-#'  \item{NDE_control]}{Direct effect estimate when the exposure level is set to the control value.}
-#'  \item{NDE_treated]}{Direct effect estimate when the exposure level is set to the treated value.}
-#'  \item{NIE_control]}{mediated effect estimate when the exposure level is set to the control value.}
-#'  \item{NIE_treated]}{mediated effect estimate when the exposure level is set to the treated value.}
-#'  \item{ANDE}{Average direct effect of X on Y}
-#'  \item{ANIE}{Average indirect effect of X on Y}
-#'  \item{TE}{The total effect of A on Y.}
-#'}
-#' The user may specify \code{pars = NULL} to display all estimated parameters where all chaines merged.
-#' With this argument, \code{bayesgmed_summary()} prints the all parameters defined in the model definations. But the most important ones are:
-#'
-#' \describe{
-#'  \item{alphaZ[]}{Prameter estimate of the confounders (i.e., X -> Y) relationship listed in the order they are specified in covarietes in \code{bayesgmed()}. alpha[1] is the intercept.}
-#'  \item{alphaM}{Prameter estimate of the M -> Y relationship.}
-#'  \item{alphaA}{Prameter estimate of the A -> Y relationship.}
-#'  \item{betaZ}{Prameter estimate of the the confounders (i.e., X -> M) relationship listed in the order they are specified in covarietes \code{bayesgmed()}. beta[1] is the intercept.}
-#'  \item{betaA}{Prameter estimate of the A -> M relation ship.}
-#'  \item{NDE_control]}{Direct effect estimate when the exposure level is set to the control value.}
-#'  \item{NDE_treated]}{Direct effect estimate when the exposure level is set to the treated value.}
-#'  \item{NIE_control]}{mediated effect estimate when the exposure level is set to the control value.}
-#'  \item{NIE_treated]}{mediated effect estimate when the exposure level is set to the treated value.}
-#'  \item{ANDE}{Average direct effect of X on Y}
-#'  \item{ANIE}{Average indirect effect of X on Y}
-#' #'}
-
 #' To learn more about the additional parameters, refer to the Stan code
 #' (\code{cat(get_stancode(fit))}).
 #'}
@@ -75,7 +49,7 @@ bayesgmed_summary <- function(
     ){
 
     # Check that mod is a Stanfit object
-    if (!(class(model) == "stanfit")) stop("Model is not a stanfit object.")
+    if (!inherits(model, "stanfit")) stop("Model is not a stanfit object.")
 
     # Choose which parameters to display
     if (is.null(pars)) pars <- model@sim$pars_oi  # Return all parameters
