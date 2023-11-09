42b8bc8ae89cb38098cbe9902ec4d096f86093f3
Belay <belaybirlie.yimer@manchester.ac.uk>
2023-11-09 09:59:37 +0000

    tidying rd file

--- R/bayesgmed.R
+++ R/bayesgmed.R
@@ -50,7 +50,8 @@
 #' 
 #' # With priors
 #' P <- 3 # number of covariates plus the intercept term 
-#' priors <- list(scale_m = 2.5*diag(P+1), scale_y = 2.5*diag(P+2), location_m = rep(0, P+1), location_y = rep(0, P+2))
+#' priors <- list(scale_m = 2.5*diag(P+1), scale_y = 2.5*diag(P+2), 
+#' location_m = rep(0, P+1), location_y = rep(0, P+2))
 #' fit1 <- bayesgmed(outcome = "Y", mediator =  "M", treat = "A", covariates = c("Z1", "Z2"), 
 #' dist.y = "binary", dist.m = "binary", link.y = "logit", link.m = "logit", priors = priors,  
 #' data = example_data)
